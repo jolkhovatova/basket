d7357135e22b23d6ae4a113c6ab61b57eaf3ef80
Julia <jolkhovatova@gmail.com>
2019-11-06 02:24:02 +0200

    регистрация

--- auth.php
+++ auth.php
@@ -1,5 +1,16 @@
-<form method="post" class="form-inline mx-3">
-    <input type="text" class="form-control" placeholder="Login" name="login">
-    <input type="password" class="form-control" placeholder="Password" name="password">
-    <button  class="btn">ok</button>
-</form>
+<?php define('DISABLE_AUTH_FORM', false); ?>
+
+<?php if (DISABLE_AUTH_FORM===false): ?>
+    <form method="post" class="form-inline mx-3">
+        <input type="text" class="form-control" placeholder="Login" name="login">
+        <input type="password" class="form-control" placeholder="Password" name="password">
+        <button class="btn">ok</button>
+    </form>
+
+    <div>
+        <a href="auth-page.php">
+            Регистрация
+        </a>
+    </div>
+<?php endif; ?>
+
